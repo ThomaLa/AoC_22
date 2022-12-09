95a6f071fffcb3f004325130a66a95d490476828
Thomas Lapôtre <tlapotre@google.com>
2022-12-09 19:54:28 +0100

    Simplify Day 9 subfunctions.

--- day__9.jl
+++ day__9.jl
@@ -1,27 +1,20 @@
 include("base.jl")
 
-function follow(head, tail)
-  diff = head - tail
-  abs(diff[1] * diff[2]) == 2 ? min.(max.(diff, -1), 1) : diff .÷ 2
-end
+follow((h, t)) = maximum(abs.(h - t)) > 1 ? sign.(h - t) : [0, 0]
 
 open(Personal.to_path(ARGS[1]), "r") do input
   size = parse(Int, ARGS[2])
   positions = [[0, 0] for _ in 1:size]
   visited = Set()
   directions = Dict('U'=>[0,1],'D'=>[0,-1],'R'=>[1,0],'L'=>[-1,0])
-
-  function move!(knot, direction)
-    positions[knot] += direction
-    if knot < size
-      move!(knot+1, follow(positions[knot:knot+1]...))
-    else
-      push!(visited, positions[knot])
+  
+  for line in readlines(input), _ in 1:parse(Int, line[3:end])
+    positions[1] += directions[line[1]]
+    for knot in 2:size
+      positions[knot] += follow(positions[knot-1:knot])
     end
+    push!(visited, positions[end])
   end
-
-  for line in readlines(input), _ in 1:parse(Int, line[3:end]) 
-    move!(1, directions[line[1]])
-  end
-  println(length(push!(visited, [0,0])))
+  push!(visited, [0,0]) |> length |> println
 end
+
