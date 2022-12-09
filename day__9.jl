37f50d41db0348f5250d5c783c965233eb11c57a
Thomas Lapôtre <tlapotre@google.com>
2022-12-09 18:35:52 +0100

    Day 9: Snakes have some tricky moves to ensure continuity.

--- day__9.jl
+++ day__9.jl
@@ -1,7 +1,27 @@
 include("base.jl")
 
+function follow(head, tail)
+  diff = head - tail
+  abs(diff[1] * diff[2]) == 2 ? min.(max.(diff, -1), 1) : diff .÷ 2
+end
+
 open(Personal.to_path(ARGS[1]), "r") do input
-  
-  println("Part 1: ", 0)
-  println("Part 2: ", 0)
+  size = parse(Int, ARGS[2])
+  positions = [[0, 0] for _ in 1:size]
+  visited = Set()
+  directions = Dict('U'=>[0,1],'D'=>[0,-1],'R'=>[1,0],'L'=>[-1,0])
+
+  function move!(knot, direction)
+    positions[knot] += direction
+    if knot < size
+      move!(knot+1, follow(positions[knot:knot+1]...))
+    else
+      push!(visited, positions[knot])
+    end
+  end
+
+  for line in readlines(input), _ in 1:parse(Int, line[3:end]) 
+    move!(1, directions[line[1]])
+  end
+  println(length(push!(visited, [0,0])))
 end
