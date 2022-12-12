6fe259863b1e8d4a8ee3624caf08149676062332
Thomas Lapôtre <tlapotre@google.com>
2022-12-12 13:59:29 +0100

    Simplify the code: only the starting point needs to change between parts.

--- day_12.jl
+++ day_12.jl
@@ -5,14 +5,13 @@ function main()  # julia day_12.jl input part
   heights = open(input -> collect(readlines(input)), Personal.to_path(ARGS[1]))
   boundary = []
   visited = Set()
-  start, goal, check = ARGS[2] == "1" ? ('S', 'E', ≤(1)) : ('E', 'a', ≥(-1))
+  start = ARGS[2] == "1" ? 'S' : 'a'
 
   z(c) = c > 'Z' ? c : c == 'S' ? 'a' : 'z'
   for (y, line) in enumerate(heights), (x, height) in enumerate(line)
     if height == start
       push!(boundary, (0, x, y))
       push!(visited, (x, y))
-      break
     end
   end
 
@@ -22,8 +21,8 @@ function main()  # julia day_12.jl input part
       if (!((nx, ny) in visited)
           && checkbounds(Bool, heights, ny)
           && checkbounds(Bool, heights[ny], nx)
-          && check(z(heights[ny][nx]) - z(heights[y][x])))
-        if heights[ny][nx] == goal
+          && z(heights[ny][nx]) - z(heights[y][x]) ≤ 1)
+        if heights[ny][nx] == 'E'
           println(distance + 1)
           return
         end
