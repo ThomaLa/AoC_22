5668380f633e288d5854e3fd9f4a5c1c21d0d608
Thomas Lapôtre <tlapotre@google.com>
2022-12-16 23:21:58 +0100

    Broke part 2 trying to optimize it.\nOff by one, but least it's fast now.

--- day_16.jl
+++ day_16.jl
@@ -11,6 +11,12 @@ function to_valve(line)
   tokens[1], Valve(parse(Int, tokens[2]), split(tokens[3], ", "))
 end
 
+struct State
+  pos_1::String
+  pos_2::String
+  open_valves::Int
+end
+
 function main()
   valves = Dict(collect(to_valve.(
     open(input -> collect(readlines(input)),
@@ -48,8 +54,28 @@ function main()
     end
   end
 
+  bitmap(valves) = isempty(valves) ? 1 : sum(2^i for (i, room) in enumerate(keys(paths)) if room in valves)
+  seen = Dict{State, Int}()
+  best = 0
+  function ok!(seen, valves, new_flow, pos_1, pos_2)
+    if pos_1 < pos_2
+      tmp = pos_1
+      pos_1 = pos_2
+      pos_2 = pos_1
+    end
+    state = State(pos_1, pos_2, bitmap(valves))
+    if new_flow > best
+      println("$(bitstring(state.open_valves)[end-16:end]) $pos_1 $pos_2: $new_flow")
+      seen[state] = new_flow
+      best = new_flow
+      return true
+    elseif get(seen, state, 0) < new_flow
+      seen[state] = new_flow
+      return best < 1000 || 2new_flow > best
+    end
+    return false
+  end
   function explore_together(flow, open_valves, time_1, pos_1, time_2, pos_2)
-    best = max(best, flow)
     for (neigh_1, dist_1) in pairs(paths[pos_1])
       if neigh_1 in open_valves
         continue
@@ -61,24 +87,39 @@ function main()
         new_time_1 = time_1 + dist_1 
         new_time_2 = time_2 + dist_2
         if max(new_time_1, new_time_2) < 26 && neigh_1 != neigh_2
-          explore_together(
-            flow + (25 - time_1 - dist_1) * valves[neigh_1].flow_rate + (
-                    25 - time_2 - dist_2) * valves[neigh_2].flow_rate,
-            union(open_valves, [neigh_1, neigh_2]),
-            new_time_1 + 1, neigh_1,
-            new_time_2 + 1, neigh_2)
+          new_open_valves = union(open_valves, [neigh_1, neigh_2])
+          new_flow = flow + (25 - time_1 - dist_1) * valves[neigh_1].flow_rate + (
+                             25 - time_2 - dist_2) * valves[neigh_2].flow_rate
+          b = bitmap(new_open_valves)
+          if ok!(seen, new_open_valves, new_flow, neigh_1, neigh_2)
+            explore_together(
+              new_flow,
+              new_open_valves,
+              new_time_1 + 1, neigh_1,
+              new_time_2 + 1, neigh_2)
+          end
         elseif new_time_1 < 26
-          explore_together(
-            flow + (25 - time_1 - dist_1) * valves[neigh_1].flow_rate,
-            union(open_valves, [neigh_1]),
-            new_time_1 + 1, neigh_1,
-            time_2, pos_2)
+          new_open_valves = union(open_valves, [neigh_1])
+          new_flow = flow + (25 - time_1 - dist_1) * valves[neigh_1].flow_rate
+          b = bitmap(new_open_valves)
+          if ok!(seen, new_open_valves, new_flow, neigh_1, pos_2)
+            explore_together(
+              new_flow,
+              new_open_valves,
+              new_time_1 + 1, neigh_1,
+              time_2, pos_2)
+          end
         elseif new_time_2 < 26
-          explore_together(
-            flow + (25 - time_2 - dist_2) * valves[neigh_2].flow_rate,
-            union(open_valves, [neigh_2]),
-            time_1, pos_1,
-            new_time_2 + 1, neigh_2)
+          new_open_valves = union(open_valves, [neigh_2])
+          new_flow = flow + (25 - time_2 - dist_2) * valves[neigh_2].flow_rate
+          b = bitmap(new_open_valves)
+          if ok!(seen, new_open_valves, new_flow, pos_1, neigh_2)
+            explore_together(
+              new_flow,
+              new_open_valves,
+              time_1, pos_1,
+              new_time_2 + 1, neigh_2)
+          end
         end
       end
     end
